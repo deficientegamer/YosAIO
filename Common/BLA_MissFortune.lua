8ef5573a1b1cf625082cec1ccf66e91423cacc36
Deficiente Gamer <deficientegamerbr@gmail.com>
2020-02-01 23:32:35 -0300

    fx

--- Common/BLA_MissFortune.lua
+++ Common/BLA_MissFortune.lua
@@ -146,26 +146,22 @@ function MissFortune:Combo()
     local count = self:GetTargetInRange(420, hero) -- inimigos proximo ao alvo
     if count >=self.Menu.combo.minComboR:Value() and IsValid(hero)
       and Ready(_R) then
-
-      if self.Menu.combo.R:Value()
+    
+      if self.Menu.combo.R:Value() 
         and (numAround >= self.Menu.combo.minComboR:Value() or RDmg/1.6 > hero.health)  then
+        _G.SDK.Orbwalker:SetMovement(false) -- Stop moviment in R
+        inUlt=true
+        Control.CastSpell(HK_R, hero)
+        lastR = GetTickCount()
 
-        local Pred = GetGamsteronPrediction(target, self.R, myHero)
-        if Pred.Hitchance >= _G.HITCHANCE_HIGH then
-          _G.SDK.Orbwalker:SetMovement(false) -- Stop moviment in R
-          inUlt=true
-          Control.CastSpell(HK_R, Pred.CastPosition)
-          lastR = GetTickCount()
-
-          -- MOV AFTER 3 + 0.20
-          DelayAction(
-            function()
-              _G.SDK.Orbwalker:SetMovement(true)
-              inUlt=false
-            end, 3 + 0.20
-          )
-          return
-        end
+        -- MOV AFTER 3 + 0.20
+        DelayAction(
+          function()
+            _G.SDK.Orbwalker:SetMovement(true)
+            inUlt=false
+          end, 3 + 0.20
+        )
+        return
       end
     end
   end
@@ -175,23 +171,20 @@ end
 
 function MissFortune:Harass()
   local targetE = nil
-  target = self:GetTarget(650)
-  if  IsValid(target) then
-    if self.Menu.combo.E:Value()  and lastE +180 and Ready(_E) then
-      local Pred = GetGamsteronPrediction(target, self.E, myHero)
-      if Pred.Hitchance >= _G.HITCHANCE_HIGH then
-        if target and self.Menu.combo.E:Value() then
-          Control.CastSpell(HK_E,Pred.CastPosition)
-        end
-      end
-    end
-    local Pred = GetGamsteronPrediction(target, self.Q, myHero)
-    if Pred.Hitchance >= _G.HITCHANCE_HIGH then
-      if self.Menu.harass.Q:Value() then
-        Control.CastSpell(HK_Q, Pred.CastPosition)
-        lastQ = GetTickCount()
-        return
-      end
+  local  targetE = self:GetTarget(1000)
+  if self.Menu.harass.E:Value() and targetE and Ready(_E) and IsValid(targetE) then
+     Control.CastSpell(HK_E,target)
+  end
+
+
+
+  local target = self:GetTarget(self.Q.range, true)
+
+  if IsValid(target) then
+    if self.Menu.harass.Q:Value() then
+      Control.CastSpell(HK_Q, target)
+      lastQ = GetTickCount()
+      return
     end
   end
 
@@ -240,14 +233,14 @@ function MissFortune:LastHit()
           if self.Menu.lastHit.Q:Value()
             and Ready(_Q)  then
 
-            local Dmg = getdmg("Q", minion, myHero, 1)
-            if (Dmg > minion.health) then
+            local WDmg = getdmg("Q", minion, myHero, 1)
+            if (WDmg > minion.health) then
               Control.CastSpell(HK_Q, minion.pos)
               return
             end
 
-            local Dmg = getdmg("E", minion, myHero, 1)
-            if self.Menu.lastHit.E:Value() and (Dmg > minion.health) then
+            local WDmg = getdmg("E", minion, myHero, 1)
+            if self.Menu.lastHit.E:Value() and (WDmg > minion.health) then
               Control.CastSpell(HK_Q, minion.pos)
               return
             end
@@ -291,7 +284,7 @@ end
 function MissFortune:GetTarget(range, list)
   local targetList = {}
   local inputList = list or Enemys
-
+  
   for i = 1, #Enemys do
     local hero = Enemys[i]
     if GetDistanceSquared(hero.pos, myHero.pos) < range * range and IsValid(hero) then
